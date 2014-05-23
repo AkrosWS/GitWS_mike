4b976cc19dfa5af0e5ec6e58e7cabd9174fa43a5
mike <mike@iceweb.ch>
2014-05-23 10:13:34 +0200

    release 2.0

---
+++ GitWS_mike/src/AgeOutput.java
@@ -0,0 +1,5 @@
+public class AgeOutput {
+	public static void myAge(int age) {
+		System.out.println("my age " + age);
+	}
+}
--- GitWS_mike/src/Home.java
+++ GitWS_mike/src/Home.java
@@ -2,5 +2,7 @@ public class Home {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("mike");
+		
+		AgeOutput.myAge(20);
 	}
 }
